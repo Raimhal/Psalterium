7e1944f070c7202ad7ce7f31815d23950171d683
Raimhal <kormish2003@gmail.com>
2022-05-27 22:13:59 +0300

    added submodules

--- back-end/routes/catalog/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from . import books, genres, orders, basket
-
-router = APIRouter(prefix='/catalog')
-router.include_router(books.router)
-router.include_router(genres.router)
-router.include_router(orders.router)
-router.include_router(basket.router)
--- front-end/src/router/instance.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
-});
